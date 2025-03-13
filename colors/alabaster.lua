d9b22993e53390cddbc27b6ebe759d26470f6850
iddixx <125816906+ONMUQU@users.noreply.github.com>
2025-03-13 21:58:29 +0200

    StorageClass is now def_fg color and bold

--- colors/alabaster.lua
+++ colors/alabaster.lua
@@ -153,7 +153,7 @@ if vim.o.background == "dark" then
         Macro = { fg = "#54699C" },
         PreCondit = { fg = "#54699C" },
         Type = { fg = punct_fg },
-        StorageClass = { fg = ansi.white },
+        StorageClass = { fg = def_fg, bold = true },
         Structure = { fg = def_fg, bold = true },
         Typedef = { fg = ansi.white },
 
